fd2822f1c6e6916ac08c6ca8bd966b91bf4c7295
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2024-02-20 11:49:08 +0300

    fix(utils/get-text-from-file): add trim for start of string

--- utils/get-text-from-file.js
+++ utils/get-text-from-file.js
@@ -1,7 +1,12 @@
 import fs from "fs";
 
+const NO_RUSSIAN_LETTERS_PATTERN = /[^а-яА-Я] /g;
+
 export const getTextFromFile = (path) => {
-  const text = fs.readFileSync(path, "utf-8").replace(/[^а-яА-Я] /g, "");
+  const text = fs
+    .readFileSync(path, "utf-8")
+    .trimStart()
+    .replace(NO_RUSSIAN_LETTERS_PATTERN, "");
 
   return text;
 };
